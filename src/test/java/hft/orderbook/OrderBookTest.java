bd24aa6c2a0782c287a2153e6c1da77d26577bee
Lena <umpalus@gmail.com>
2025-05-13 09:06:05 +0300

    Initial commit

--- src/test/java/hft/orderbook/OrderBookTest.java
+++ src/test/java/hft/orderbook/OrderBookTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderBookTest {
-    int a;
 
     @Test
     void testNoMatchingWhenPricesDoNotCross() {
